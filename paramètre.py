272bdb261b7af523b8480651ce050785931fa62f
MatY0da <mathis.lharidon@gmail.com>
2023-04-04 16:31:57 +0200

    Début du jeu

---
+++ paramètre.py
@@ -0,0 +1,19 @@
+# Fichier Setting.py qui contient toutes les constantes du jeu
+
+# Importation
+import pygame
+pygame.init()
+
+# Les couleurs
+blanc = (255, 255, 255)
+noir = (0, 0, 0)
+bleu = (0, 0, 255)
+vert = (0, 255, 0)
+couleur_fond = (255, 0, 0)
+
+# La taille des boutons
+bouton_largeur = 300
+bouton_hauteur = 50
+
+# Créer une police de caractères
+police = pygame.font.Font(None, 36)
